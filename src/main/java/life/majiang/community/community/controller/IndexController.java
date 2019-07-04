b194eead37c607e0b00e6df16f5ef381dabff4c3
Never-mind <542732342@qq.com>
2019-07-05 01:43:27 +0800

    first commit

---
+++ src/main/java/life/majiang/community/community/controller/IndexController.java
@@ -0,0 +1,10 @@
+package life.majiang.community.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index() { return "index"; }
+}
